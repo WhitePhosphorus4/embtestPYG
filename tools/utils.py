21bb7240f2ffa8552f4ce2c3a9ecd044c0e705a2
xionghao <1038478003@qq.com>
2022-09-24 17:24:28 +0800

    add normalize function

--- tools/utils.py
+++ tools/utils.py
@@ -3,6 +3,8 @@
 # import torch
 import networkx as nxc
 import matplotlib.pyplot as plt
+import numpy as np 
+import scipy.sparse as sp
 
 
 def visualize_graph(G, color = "b"):
@@ -24,10 +26,11 @@ def visualize_embedding(h, color, epoch=None, loss=None):
     主要可视化完成embedding的图结果，可以输入epoch和loss，用于显示训练过程
     '''
     plt.figure(figsize=(7,7))
-    plt.xticks([])
-    plt.yticks([])
     h = h.detach().cpu().numpy()
+    h = normalize(h)
     plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
+    plt.xticks(np.arange(-1, 1.1, 0.4))
+    plt.yticks(np.arange(-1, 1.1, 0.4))
     if epoch is not None and loss is not None:
         plt.xlabel(f'Epoch: {epoch}, Loss: {loss.item():.4f}', fontsize=16)
     plt.show()
@@ -41,3 +44,52 @@ def accuracy(output, labels):
     correct = preds.eq(labels).double()
     correct = correct.sum()
     return correct / len(labels)
+
+
+def normalize(h):
+    '''
+    Normalize the matrix h by max-min normalization.
+    '''
+    for i in range(h.shape[1]):
+        t = h[:, i]
+        max_t = t.max()
+        h[:, i] = t / max_t
+    return h
+
+
+def normalize_maxmin(Mx, axis=2):
+    '''
+    Normalize the matrix Mx by max-min normalization.
+    axis=0: normalize each row
+    axis=1: normalize each column
+    axis=2: normalize the whole matrix
+    '''
+    if axis == 1:
+        M_min = np.min(Mx, axis=1)
+        M_max = np.max(Mx, axis=1)
+        for i in range(Mx.shape[1]):
+            Mx[:, i] = (Mx[:, i] - M_min) / (M_max - M_min)
+    elif axis == 0:
+        M_min = np.min(Mx, axis=0)
+        M_max = np.max(Mx, axis=0)
+        for i in range(Mx.shape[0]):
+            Mx[i, :] = (Mx[i, :] - M_min) / (M_max - M_min)
+    elif axis == 2:
+        M_min = np.amin(Mx)
+        M_max = np.amax(Mx)
+        Mx = (Mx - M_min) / (M_max - M_min)
+    else:
+        print('Error')
+        return None
+    return Mx
+
+def normalize_Zscore(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+    r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+    mx.dot(r_mat_inv_sqrt)
+    mx.transpose()
+    mx.dot(r_mat_inv_sqrt)
+    return mx
--- training_test.py
+++ training_test.py
@@ -9,14 +9,14 @@ import torch.optim as optim
 from tools.models import GCN
 from torch_geometric.datasets import KarateClub, Planetoid
 from tools.utils import visualize_graph, visualize_embedding, accuracy
-from IPython.display import Javascript  # Restrict height of output cell.
-displayhook(Javascript('''google.colab.output.setIframeHeight(0, true, {maxHeight: 430})'''))
+# from IPython.display import Javascript  # Restrict height of output cell.
+# displayhook(Javascript('''google.colab.output.setIframeHeight(0, true, {maxHeight: 430})'''))
 
 max_epoch = 5001
 datasets = KarateClub()
 # dataset = Planetoid(root='./dataset/Cora', name='Cora')
 graph_data = datasets[0]
-model = GCN(num_features=datasets.num_features, num_classes=datasets.num_classes, set_seed=False)
+model = GCN(num_features=datasets.num_features, num_classes=datasets.num_classes, set_seed=True, rd_seed=1444)
 
 # criterion = torch.nn.CrossEntropyLoss()
 # criterion = torch.nn.functional.nll_loss()
@@ -37,7 +37,7 @@ def train(data, epoch):
 
 for epoch in range(max_epoch):
     loss, h = train(graph_data, epoch)
-    if epoch % 1000 == 0:
+    if epoch % 1000 == 0 and epoch != 0:
         visualize_embedding(h, color=graph_data.y, epoch=epoch, loss=loss)
         # time.sleep(0.3)
 
